ecfd987c8abf56c539b45d5af4b74ed78a885a62
Nedaa Gehad <nedaagehd@gmail.co>
2024-02-16 23:51:18 +0200

    add game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Mole from "./components/Mole";
 
 function App() {
+  const [randomHoleIndex, setRandomHoleIndex] = useState<number | null>();
+  const [stopMole, setStopMole] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setRandomHoleIndex(Math.floor(Math.random() * 9));
+    }, 500);
+
+    if (stopMole) clearInterval(interval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [stopMole]);
+
+  const displayMole = (holeIndex: number) => {
+    if (holeIndex === randomHoleIndex)
+      return (
+        <div 
+        style={{ position: "absolute", top: 0, left: "30px"}} 
+        
+        onMouseDown={() => setScore(score + 1)}>
+          <Mole />
+        </div>
+      );
+  };
+
+  const displayHoles = () => {
+    let arrayOfHoles = [];
+    for (let i = 0; i < 9; i++) {
+      arrayOfHoles.push(
+        <div className="hole-wapper" style={{ position: "relative", height: "80%", display: "flex", flexDirection: "column", justifyContent: "flex-end", alignItems: "center", overflow: "hidden"}} id={i.toString()} key={i}>
+          {displayMole(i)}
+          <div className="hole"/>
+        </div>
+      );
+    }
+
+    return arrayOfHoles;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>score: {score * 10}</h2>
+      <div className="board">{displayHoles().map((item) => item)}</div>
+      <button onClick={() => setStopMole(true)}>Stop!!!</button>
     </div>
   );
 }
---
+++ src/components/Mole/index.tsx
@@ -0,0 +1,14 @@
+
+const Mole = () => {
+  return (
+    <div style={{ width: "120px", height: "130px", background: "#764a2c", borderRadius: "45% 45% 0 0", position: "relative"}}>
+        <div className="eye" style={{ width: "10px", height: "10px", background: "#000", borderRadius: "50%", position: "absolute", left: "30px", top: "30px"}}></div>
+        <div className="eye" style={{ width: "10px", height: "10px", background: "#000", borderRadius: "50%", position: "absolute", right: "30px", top: "30px"}}></div>
+        <div className="nose" style={{ width: "30px", height: "30px", background: "#000", borderRadius: "50%", position: "absolute", right: "45px", top: "40px", zIndex: 2}}></div>
+        <div className="lips" style={{ width: "40px", height: "40px", background: "#fdba95", borderRadius: "50%", position: "absolute", right: "25px", top: "50px", zIndex: 1}}></div>
+        <div className="lips" style={{ width: "40px", height: "40px", background: "#fdba95", borderRadius: "50%", position: "absolute", left: "25px", top: "50px", zIndex: 1}}></div>
+    </div>
+  )
+}
+
+export default Mole
