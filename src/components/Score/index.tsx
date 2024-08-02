5757e62456099433e8d8181217e2bf80b77f5300
Nedaa Gehad <nedaagehd@gmail.co>
2024-08-02 18:41:34 +0300

    Refactor score component

--- src/components/Score/index.tsx
+++ src/components/Score/index.tsx
@@ -1,10 +1,16 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
+import {ReactComponent as StarIcon} from "../../assets/images/star.svg"
 import "./utils/styles.css"
 
 const Score = () => {
   const { score } = useSelector((state: RootState) => state.score);
-  return <h1 className="score">Score: {score}</h1>;
+  return <div className="score-box">
+    <StarIcon className="star" />
+    <div className="score-text lilita-one-font">
+      {score}
+    </div>
+  </div>;
 };
 
 export default Score;
