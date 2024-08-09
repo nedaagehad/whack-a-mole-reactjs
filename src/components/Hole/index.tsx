b0611c2bac5687a5a16e8c1f6b30da04efe2d5e0
Nedaa Gehad <nedaagehd@gmail.co>
2024-08-09 12:16:28 +0300

    Change hole design

--- src/components/Hole/index.tsx
+++ src/components/Hole/index.tsx
@@ -1,8 +1,13 @@
 import { IHoleProps } from "./utils/types";
-import "./utils/styles.css"
+import "./utils/styles.css";
+import {ReactComponent as HoleBack} from "../../assets/images/hole-back.svg"
+import {ReactComponent as HoleFront} from "../../assets/images/hole-front.svg"
 
 const Hole = ({ index }: IHoleProps) => {
-  return <div className="hole" id={index.toString()} />;
+  return <div className="hole" id={index.toString()}>
+    <HoleBack width={"100%"} style={{ position: "absolute", top: 0, zIndex: 1}} />
+    <HoleFront width={"100%"} style={{ position: "absolute", top: "25px", zIndex: 3}} />
+  </div>;
 };
 
 export default Hole;
