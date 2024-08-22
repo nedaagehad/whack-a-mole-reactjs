d29ca0e877730b6ae6b411c6f210fd87a670ae63
Nedaa Gehad <nedaagehd@gmail.co>
2024-08-22 18:32:00 +0300

    Fix: random index repeatition issue

--- src/utils/GenerateRandomIndex.ts
+++ src/utils/GenerateRandomIndex.ts
@@ -2,7 +2,8 @@ const generateRandomIndex = (prevIndex: number | null) => {
   let randomIndex = Math.floor(Math.random() * 9);
 
   if (randomIndex === prevIndex) {
-    randomIndex = Math.floor(Math.random() * 9);
+    // randomIndex = Math.floor(Math.random() * 9);
+    randomIndex = generateRandomIndex(prevIndex)
   }
   return randomIndex;
 };
